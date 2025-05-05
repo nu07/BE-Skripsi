d7fcd9a44de871daef3a0bc9b776c471765acb8b
nu_vandalstik <wisnukristanto321@gmail.com>
2025-05-05 20:57:14 +0700

    feat CRUD admin, create mahasiswa etc

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,21 +1,14 @@
-// routes/index.ts
 import { Router } from 'express';
-import mahasiswaRoutes from './routes/mahasiswa';
-import dosenRoutes from './routes/dosen';
 import adminRoutes from './routes/admin';
-import skripsiRoutes from './routes/skripsi';
-import pendaftaranSidangRoutes from './routes/pendaftaranSidang';
-import authRoutes from './routes/auth';
-import NewsRoute from './routes/news';
+// import authRoutes from './routes/auth.route';
+// import mahasiswaRoutes from './routes/mahasiswa.route';
+// import dosenRoutes from './routes/dosen.route';
 
 const router: Router = Router();
 
-router.use('/auth', authRoutes);
-router.use('/mahasiswa', mahasiswaRoutes);
-router.use('/dosen', dosenRoutes);
-router.use('/admin', adminRoutes);
-router.use('/skripsi', skripsiRoutes);
-router.use('/pendaftaran-sidang', pendaftaranSidangRoutes);
-router.use('/news', NewsRoute);
+router.use('/', adminRoutes);
+// router.use('/auth', authRoutes);
+// router.use('/mahasiswa', mahasiswaRoutes);
+// router.use('/dosen', dosenRoutes);
 
 export default router;
